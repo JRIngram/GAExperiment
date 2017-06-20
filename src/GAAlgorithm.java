cbe59592473c76d4d71e647456a323c2335b67db
JRIngram <jr-ingram@hotmail.co.uk>
2017-06-20 21:41:53 +0100

    Add GAAlgorithm file

---
+++ src/GAAlgorithm.java
@@ -0,0 +1,45 @@
+
+public class GAAlgorithm{
+
+	private String target;
+	private Population population;
+	private int generationNum;
+	
+	public GAAlgorithm(String target) {
+		this.target = target;
+		generationNum = 1;
+	}
+	
+	public void startAlgorithm(){
+		System.out.println("Generating initial population...");
+		this.population = new Population(12, target.length(), target);
+		System.out.println("Initial population generated!");
+	}
+	
+	public String generationMessage(){
+		String generationMessage = "";
+		generationMessage = "Generation: " + generationNum;
+		generationMessage += "\n" + population.toString();
+		generationMessage += "\n" + "Fittest member: " + population.getFittestMember().toString();
+		generationMessage += "\n" + "Least fit member: " + population.getLeastFitMember().toString();
+		generationMessage += "\n";
+		return generationMessage;
+	}
+	
+	public boolean checkIfTargetFound(){
+		if(population.getFittestMember().getFitness() == 0){
+			return false;
+		}
+		else{
+			return true;
+		}
+	}
+	
+	public void runGeneration(){
+		generationNum++;
+		population.createNextGeneration(population.getFittestHalf());
+	}
+	
+}
+
+
